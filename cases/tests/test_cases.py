48c548feeee651a7f43fc1b0db6d26fe00b6b3e5
Sergei Chipiga <schipiga@mirantis.com>
2016-11-03 17:38:38 +0200

    Expand testcase scenarios in autotests

--- cases/tests/test_cases.py
+++ cases/tests/test_cases.py
@@ -10,8 +10,10 @@ cases, according to the convenience of automation.**
 These testcases are implemented with POM framework
 (http://pom.readthedocs.io/en/latest/, https://github.com/sergeychipiga/pom)
 and STEPS-methodology.
+
 If you are interesting how POM works in enterprise testing, please visit
 https://github.com/Mirantis/mos-horizon/tree/v9.1.
+
 If you are curious about STEPS-methodology in enterprise testing, please visit
 http://stepler.readthedocs.io/, https://github.com/Mirantis/stepler.
 
@@ -52,8 +54,7 @@ class TestTop250Imdb(object):
     """
 
     def test_share_with_social(self, top_movies_steps):
-        """**Scenario:** Unauthorized user can share top IMDB movies with
-        social networks.
+        """**Scenario:** User can share top IMDB movies with social networks.
 
         **Setup:**
 
@@ -71,18 +72,99 @@ class TestTop250Imdb(object):
         top_movies_steps.share_with_social()
 
     def test_like_movie(self, top_movies_steps):
+        """**Scenario:** If user likes movie, it leads to login page.
+
+        **Setup:**
+
+        #. Open top IMDB page.
+
+        **Steps:**
+
+        #. Like movie in movies list.
+        #. Check page login page is opened.
+
+        **Teardown:**
+
+        #. Close browser.
+        """
         top_movies_steps.like_movie(movie_number=0)
 
     def test_add_movie_to_watch_list(self, top_movies_steps):
+        """**Scenario:** If user adds movie to watch list, it leads to login
+                         page.
+
+        **Setup:**
+
+        #. Open top IMDB page.
+
+        **Steps:**
+
+        #. Add movie in movies list to watch list.
+        #. Check page login page is opened.
+
+        **Teardown:**
+
+        #. Close browser.
+        """
         top_movies_steps.add_movie_to_watch_list(movie_number=0)
 
     def test_open_movie_details_by_title(self, top_movies_steps):
+        """**Scenario:** Detailed info movie page is opened when user clicks
+                         title of movie.
+
+        **Setup:**
+
+        #. Open top IMDB page.
+
+        **Steps:**
+
+        #. Click movie title.
+        #. Check that page with detailed info about movie is opened.
+
+        **Teardown:**
+
+        #. Close browser.
+        """
         top_movies_steps.show_movie_details(movie_number=0)
 
     def test_open_movie_details_by_icon(self, top_movies_steps):
+        """**Scenario:** Detailed info movie page is opened when user clicks
+                         icon of movie.
+
+        **Setup:**
+
+        #. Open top IMDB page.
+
+        **Steps:**
+
+        #. Click movie icon.
+        #. Check that page with detailed info about movie is opened.
+
+        **Teardown:**
+
+        #. Close browser.
+        """
         top_movies_steps.show_movie_details(movie_number=0, by_title=False)
 
     def test_reverse_movies_sort_order(self, top_movies_steps):
+        """**Scenario:** Clicking on "Reverse sort" button leads to reverse of
+                         movies list.
+
+        **Setup:**
+
+        #. Open top IMDB page.
+
+        **Steps:**
+
+        #. Click "Reverse sort" button.
+        #. Check that movies list is reversed.
+        #. Click "Reverse sort" button.
+        #. Check that movies list is reversed.
+
+        **Teardown:**
+
+        #. Close browser.
+        """
         top_movies_steps.reverse_movies_sort_order()
         top_movies_steps.reverse_movies_sort_order()
 
@@ -92,9 +174,41 @@ class TestTop250Imdb(object):
                                            config.SORT_NUBMER_RATINGS,
                                            config.SORT_YOUR_RATING])
     def test_change_movies_sort_type(self, top_movies_steps, sort_type):
+        """**Scenario:** Changing of sort type leads to reordering of movies
+                         list.
+
+        **Setup:**
+
+        #. Open top IMDB page.
+
+        **Steps:**
+
+        #. Change sort type of movies list.
+        #. Check that movies list order was changed.
+        #. Click "Reverse sort" button.
+        #. Check that movies list is reversed.
+
+        **Teardown:**
+
+        #. Close browser.
+        """
         top_movies_steps.change_movies_sort_type(sort_type)
         top_movies_steps.reverse_movies_sort_order()
 
     @pytest.mark.parametrize('movies_amount', [250])
     def test_movies_list_amount(self, top_movies_steps, movies_amount):
+        """**Scenario:** Movies list contains exaclty 250 items.
+
+        **Setup:**
+
+        #. Open top IMDB page.
+
+        **Steps:**
+
+        #. Check that movies list contains exaclty 250 items.
+
+        **Teardown:**
+
+        #. Close browser.
+        """
         top_movies_steps.check_movies_list_has_length(movies_amount)
